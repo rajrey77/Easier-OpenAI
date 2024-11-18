62b121adc10bee85ee342dffe2cb84ea38efabd9
rajrey77 <rajesh.a.pradhan@capgemini.com>
2024-11-17 19:57:27 -0600

    Update package folder

--- package/dist/index.js
+++ package/dist/index.js
@@ -1,32 +1,45 @@
 import openai from 'openai';
 import 'dotenv/config';
 import { writeFile } from 'fs/promises';
-writeFile('../../.env', `# Define your OpenAI API key here like this: OAI_API_KEY="your-api-key"`);
 
 const gconfig = {
-    apiKey: process.env.OAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY
 };
 
 const client = new openai.OpenAI(gconfig);
 
-console.log(openai);
+function getResponse(config, test = false) {
+    const newPromise = new Promise(async (resolve, reject) => {
+        try {
+            if(config.type === 'chat') {
+                function changeMsgs(msgs) {
+                    let newMsgs = msgs.toSpliced(0, 0, { role: 'system', content: config.sysInstructions });
+                    newMsgs.push({ role: 'user', content: config.prompt });
+                    console.log(newMsgs);
+                    return newMsgs;
+                }
+                let messages = config.messages;
+                messages.push({ role: 'user', content: config.prompt });
 
-function getResponse(config) {
-    if(config.type === 'chat') {
-        return client.chat.completions.create({
-            engine: config.model,
-            messages: config.sysInstructions != "" ? 
-            config.messages
-                .toSpliced(0, 0, { role: 'system', content: config.sysInstructions })
-                .push({ role: 'user', content: config.message }) :
-            config.messages
-                .push({ role: 'user', content: config.message })
-            ,
-            max_tokens: config.maxTokens,
+                let response;
+                response = await client.chat.completions.create({
+                    model: config.model,
+                    messages: config.sysInstructions == "" ? messages : changeMsgs(config.messages),
+                    max_tokens: config.maxTokens,
+                });
+                resolve(response);
+            }
+        } catch(e) {
+            reject(e);
+        }
+    });
+    if(test) {
+        return newPromise;
+    } else {
+        return newPromise.then((response) => {
+            return response.choices[0].message.content;
         });
     }
 }
 
-module.exports = {
-    getResponse
-};
+export { getResponse };
